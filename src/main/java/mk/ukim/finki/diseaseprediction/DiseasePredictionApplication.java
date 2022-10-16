701156c77aba98b1275daaea5437b6658667046f
magdalena <magdalena@elevate-global.biz>
2022-10-16 21:03:00 +0200

    Initial commit

---
+++ src/main/java/mk/ukim/finki/diseaseprediction/DiseasePredictionApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.diseaseprediction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiseasePredictionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiseasePredictionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/ukim/finki/diseaseprediction/DiseasePredictionApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.diseaseprediction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiseasePredictionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
